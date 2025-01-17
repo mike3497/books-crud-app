20114df6f7c7bb22943d09366888d40a29ee0746
Michael Carillon <mcarillon97@gmail.com>
2025-01-16 23:23:22 -0500

    feat: use env for api base

--- src/services/booksService.ts
+++ src/services/booksService.ts
@@ -4,17 +4,20 @@ import type { UpdateBookRequestDTO } from '@/models/updateBookRequestDTO';
 import axios from 'axios';
 
 export const fetchBooks = async (): Promise<BookDTO[]> => {
-  const response = await axios.get('http://localhost:3000/books');
+  const response = await axios.get(`${import.meta.env.VITE_APP_API_BASE_URL}/books`);
   return response.data;
 };
 
 export const fetchBook = async (id: string): Promise<BookDTO> => {
-  const response = await axios.get(`http://localhost:3000/books/${id}`);
+  const response = await axios.get(`${import.meta.env.VITE_APP_API_BASE_URL}/books/${id}`);
   return response.data;
 };
 
 export const createBook = async (createBookRequestDTO: CreateBookRequestDTO): Promise<BookDTO> => {
-  const response = await axios.post(`http://localhost:3000/books`, createBookRequestDTO);
+  const response = await axios.post(
+    `${import.meta.env.VITE_APP_API_BASE_URL}/books`,
+    createBookRequestDTO,
+  );
   return response.data;
 };
 
@@ -22,11 +25,14 @@ export const updateBook = async (
   id: string,
   updateBookRequestDTO: UpdateBookRequestDTO,
 ): Promise<BookDTO> => {
-  const response = await axios.put(`http://localhost:3000/books/${id}`, updateBookRequestDTO);
+  const response = await axios.put(
+    `${import.meta.env.VITE_APP_API_BASE_URL}/books/${id}`,
+    updateBookRequestDTO,
+  );
   return response.data;
 };
 
 export const deleteBook = async (id: string): Promise<void> => {
-  const response = await axios.delete(`http://localhost:3000/books/${id}`);
+  const response = await axios.delete(`${import.meta.env.VITE_APP_API_BASE_URL}/books/${id}`);
   return response.data;
 };
