723c01ab2ec6c6bdb08f97eae44585f6c5952298
Michael Carillon <mcarillon97@gmail.com>
2025-01-19 20:29:52 -0500

    feat: update button styling

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="flex flex-row gap-1 items-center bg-blue-500 text-white px-4 py-2 hover:bg-blue-600"
+    class="flex flex-row gap-1 items-center bg-blue-500 text-white px-5 py-3 hover:bg-blue-600 rounded-lg"
     type="button"
   >
     <slot></slot>
