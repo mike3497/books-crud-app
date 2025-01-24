9657d73be00582a99472476ea5a454b7f5640b35
Michael Carillon <mcarillon97@gmail.com>
2025-01-23 22:50:58 -0500

    feat: adjust footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,8 @@
 <template>
-  <footer class="bg-blue-950 text-white h-32 flex flex-row justify-center items-center">
-    <a class="text-sm" href="https://michaelcarillon.com/">&copy;Copyright 2025 Michael Carillon</a>
+  <footer class="bg-blue-900 text-white h-32 flex items-center justify-center">
+    <p class="text-sm">
+      &copy; Copyright 2025
+      <a class="underline" href="https://michaelcarillon.com/">Michael Carillon</a>
+    </p>
   </footer>
 </template>
