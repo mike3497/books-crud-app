40e352222ceecf570ac30ab84351e16fe55f7b4f
Michael Carillon <mcarillon97@gmail.com>
2025-01-23 23:04:12 -0500

    feat: fix dropdown arrow

--- src/components/shared/SelectField.vue
+++ src/components/shared/SelectField.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="flex flex-col gap-2" :class="{ 'has-error': !!errorMessage }">
     <label class="font-bold" :for="name">{{ label }}</label>
-    <select
-      class="block w-full px-4 py-2 border border-gray-300 focus-visible:outline-none focus-visible:border-blue-500 rounded-lg"
-      :id="name"
-      :name="name"
-      :value="inputValue"
-      @input="handleChange"
-      @blur="handleBlur"
-    >
-      <slot></slot>
-    </select>
+    <div class="relative">
+      <select
+        class="block w-full px-4 py-2 border border-gray-300 focus-visible:outline-none focus-visible:border-blue-500 rounded-lg appearance-none"
+        :id="name"
+        :name="name"
+        :value="inputValue"
+        @input="handleChange"
+        @blur="handleBlur"
+      >
+        <option disabled value="">Select an option</option>
+        <slot></slot>
+      </select>
+      <div class="absolute top-1/2 right-2 transform -translate-y-1/2">
+        <ChevronDown />
+      </div>
+    </div>
     <p class="text-red-700" v-show="errorMessage">
       {{ errorMessage }}
     </p>
@@ -18,6 +24,7 @@
 </template>
 
 <script setup lang="ts">
+import { ChevronDown } from 'lucide-vue-next';
 import { useField } from 'vee-validate';
 import { toRef } from 'vue';
 
