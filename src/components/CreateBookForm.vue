3de9298ffc85754214de01416ae60a16d80f59ec
Michael Carillon <mcarillon97@gmail.com>
2025-01-27 23:19:33 -0500

    feat: add more genres

--- src/components/CreateBookForm.vue
+++ src/components/CreateBookForm.vue
@@ -9,20 +9,36 @@
       <SelectField name="genre" label="Genre" :isRequired="true">
         <option value="Biography/Autobiography">Biography/Autobiography</option>
         <option value="Children's">Children's</option>
+        <option value="Classics">Classics</option>
+        <option value="Comedy">Comedy</option>
         <option value="Contemporary">Contemporary</option>
+        <option value="Drama">Drama</option>
+        <option value="Dystopian">Dystopian</option>
+        <option value="Epic Fantasy">Epic Fantasy</option>
         <option value="Fantasy">Fantasy</option>
+        <option value="Graphic Novel">Graphic Novel</option>
         <option value="Historical Fiction">Historical Fiction</option>
-        <option value="Horror">Horror</option>
         <option value="History">History</option>
+        <option value="Horror">Horror</option>
+        <option value="Magical Realism">Magical Realism</option>
         <option value="Memoir">Memoir</option>
         <option value="Mystery">Mystery</option>
+        <option value="Non-Fiction">Non-Fiction</option>
+        <option value="Other">Other</option>
+        <option value="Paranormal">Paranormal</option>
+        <option value="Philosophical">Philosophical</option>
+        <option value="Poetry">Poetry</option>
+        <option value="Political Thriller">Political Thriller</option>
+        <option value="Religious">Religious</option>
         <option value="Romance">Romance</option>
         <option value="Science">Science</option>
         <option value="Science Fiction">Science Fiction</option>
         <option value="Self-Help">Self-Help</option>
+        <option value="Short Stories">Short Stories</option>
         <option value="Thriller">Thriller</option>
         <option value="Travel">Travel</option>
         <option value="True Crime">True Crime</option>
+        <option value="Western">Western</option>
         <option value="Young Adult (YA)">Young Adult (YA)</option>
       </SelectField>
       <TextInput name="isbn" label="ISBN" :isRequired="false" />
