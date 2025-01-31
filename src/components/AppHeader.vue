d18b14d9cfda02867700368767780d93db19d8e2
Michael Carillon <mcarillon97@gmail.com>
2025-01-30 20:22:10 -0500

    feat: link page title to home

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,9 @@
 <template>
   <header class="bg-white border-b border-gray-300">
     <div class="container mx-auto px-4 h-16 flex flex-row items-center justify-between">
-      <h1 class="font-bold text-2xl">Books CRUD App</h1>
+      <RouterLink :to="{ name: 'home' }">
+        <h1 class="font-bold text-2xl">Books CRUD App</h1>
+      </RouterLink>
     </div>
   </header>
 </template>
