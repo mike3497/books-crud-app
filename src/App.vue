1cf1af718e6adde81cfb25392328116884e7b207
Michael Carillon <mcarillon97@gmail.com>
2025-02-13 23:19:56 -0500

    feat: move header and footer into layouts folder

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script setup lang="ts">
-import AppFooter from '@/components/AppFooter.vue';
-import AppHeader from '@/components/AppHeader.vue';
+import AppFooter from '@/components/layout/AppFooter.vue';
+import AppHeader from '@/components/layout/AppHeader.vue';
 import ToastsContainer from '@/components/ToastsContainer.vue';
 </script>
